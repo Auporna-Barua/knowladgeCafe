b9a68fb57ce50be03d3aa93a7de2b35c250eb1ee
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-04-01 16:17:58 +0600

    fetch data from local json file and pass article component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/header/Header";
+import { ToastContainer } from "react-toastify";
+import { useState, useEffect } from "react";
+import Article from "./components/article/Article";
+import "react-toastify/dist/ReactToastify.css";
+import Blog from "./components/blog/Blog";
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  const [bookmarks, setBookMarks] = useState(
+    JSON.parse(localStorage.getItem("bookmark")) || []
+  );
+  const [markRead, setMarkRead] = useState(
+    JSON.parse(localStorage.getItem("readTime")) || 0
+  );
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("bookmark"));
+    if (data) {
+      setBookMarks(data);
+    }
+  }, []);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
   return (
     <div>
-    
+      <Header />
+      <div className="home">
+        <div className="left-side">
+          {blogs &&
+            blogs.map((blog) => (
+              <Article
+                key={blog.id}
+                blog={blog}
+                setBookMarks={setBookMarks}
+                bookmarks={bookmarks}
+                setMarkRead={setMarkRead}
+                markRead={markRead}
+              />
+            ))}
+        </div>
+        <div className="right-side">
+         
+        </div>
+      </div>
+      <div className="blogs">
+        <h2 className="text-center">All Question & Answer</h2>
+        <Blog />
+      </div>
+
+      <ToastContainer />
     </div>
   );
 }
